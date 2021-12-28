57fe9e55e37d9c9fe09ce8e29a28329347f37e33
unknown <muriraubach@gmail.com>
2021-12-28 17:49:27 -0300

    feat: add validation to dashboard page for authenticated users only

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 
 import { AuthContext } from "../contexts/AuthContext"
+import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
@@ -9,3 +10,9 @@ export default function Dashboard() {
     <h1>Dashboard: {user?.email}</h1>
   )
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  return {
+    props: {}
+  }
+})
