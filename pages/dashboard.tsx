24e5de6861cdd749df771adefcec9417aff35302
murilo <muriraubach@gmail.com>
2021-12-29 11:02:36 -0300

    feat: add a component to validate user roles and permissions

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react"
+import { Can } from "../components/Can"
 
 import { AuthContext } from "../contexts/AuthContext"
 import { useCan } from "../hooks/useCan"
@@ -8,14 +9,12 @@ import { withSSRAuth } from "../utils/withSSRAuth"
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
 
-  const userCanSeeMetrics = useCan({
-    roles: ['administrator']
-  })
-
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
-      {userCanSeeMetrics && <div>Métricas</div>}
+      <Can>
+        <div>Métricas</div>
+      </Can>
     </>
   )
 }
