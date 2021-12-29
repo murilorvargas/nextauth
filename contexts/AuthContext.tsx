7df4abe99bdb516b0ce402c21b405b7af2ddb44a
murilo <muriraubach@gmail.com>
2021-12-29 11:55:50 -0300

    feat: add sign out function in auth context

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -17,6 +17,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
   user: User;
   isAuthenticated: boolean;
 }
@@ -88,7 +89,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+    <AuthContext.Provider value={{ signIn, signOut, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   )
